56482e82dba4c6c4ec34c7af0abbc2c16a015d34
GvarunG <gvarun@gmail.com>
2022-02-03 17:36:22 +0530

    add alignment for some routes

--- src/Components/PatientComponent/Diagnosis.js
+++ src/Components/PatientComponent/Diagnosis.js
@@ -6,53 +6,74 @@ const Diagnosis = () => {
     <div>
       <p className="h3">Diagnosis</p>
       <Divider />
-      <div className="d-flex justify-content-evenly">
-        <div>
-          <label>Attendance</label>
+      <div className= "container px-2">
+        <div className = "row">
+         <div className="d-flex justify-content-evenly">
+          <div className="col-sm">
+           <label>Attendance</label>
 
-          <select className="selectpicker border-1 mb-1 px-4 py-1 rounded shadow">
-            <option>Select</option>
-            <option>Emergency/Acute</option>
-          </select>
-        </div>
-        <div>
-          <label>Type</label>
-          <select className="selectpicker border-1 mb-1 px-4 py-1 rounded shadow">
-            <option>Select</option>
-            <option>Provisional</option>
-          </select>
-        </div>
-        <div>
-          <label>Category</label>
-          <select className="selectpicker border-1 mb-1 px-4 py-1 rounded shadow">
-            <option>Select</option>
-            <option>Primary</option>
-          </select>
+           <select className="selectpicker border-1 mb-1 px-4 py-1 rounded shadow">
+             <option>Select</option>
+             <option>Emergency/Acute</option>
+           </select>
+          </div>
+          <div className="col-sm">
+            <label>Type</label>
+            <select className="selectpicker border-1 mb-1 px-4 py-1 rounded shadow">
+              <option>Select</option>
+              <option>Provisional</option>
+           </select>
+          </div>
+          <div className="col-sm">
+            <label>Category</label>
+            <select className="selectpicker border-1 mb-1 px-4 py-1 rounded shadow">
+              <option>Select</option>
+              <option>Primary</option>
+            </select>
+          </div>
+          </div>
         </div>
       </div>
       <br />
-      <div className="d-flex justify-content-evenly">
-        <label>Diagnosis</label>
-        <input
-          type="text"
-          className="pl-2 form-control-sm px-3   shadow-sm mb-3 bg-white rounded border"
-        />
-        <label>Country Code</label>
-        <input
-          type="text"
-          className="pl-2 form-control-sm px-3   shadow-sm mb-3 bg-white rounded border"
-        />
-        <label>ICD 10</label>
-        <input
-          type="text"
-          className="pl-2 form-control-sm px-3   shadow-sm mb-3 bg-white rounded border"
-        />
-        <label>Remarks</label>
-        <input
-          type="text"
-          className="pl-2 form-control-sm px-3   shadow-sm mb-3 bg-white rounded border"
-        />
+      <div className= "container px-2">
+        <div className = "row">
+        <div className="d-flex justify-content-evenly">
+          <div className = "col-sm">
+          <label>Diagnosis</label>
+          <input
+            type="text"
+            className="pl-2 form-control-sm px-3   shadow-sm mb-3 bg-white rounded border"
+          />
+          </div>
+          <div className="col-sm">
+          <label>Country Code</label>
+          <input
+            type="text"
+            className="pl-2 form-control-sm px-3   shadow-sm mb-3 bg-white rounded border"
+          />
+          </div>
+          <div className = "col-sm">
+          <label>ICD 10</label>
+          <input
+            type="text"
+            className="pl-2 form-control-sm px-3   shadow-sm mb-3 bg-white rounded border"
+          />
+          </div>
+          
+        </div>
+        </div>
       </div>
+      <div className="container px-2">
+        <div className="row">
+          <div className= "col-sm">
+          <label>Remarks</label>
+          <input
+            type="text"
+            className="pl-2 form-control-sm px-3   shadow-sm mb-3 bg-white rounded border"
+          />
+          </div>
+        </div>  
+      </div>   
     </div>
   );
 };
--- src/Components/PatientComponent/ReferToDoctor.js
+++ src/Components/PatientComponent/ReferToDoctor.js
@@ -4,22 +4,31 @@ const ReferToDoctor = () => {
   return (
     <div>
       <h2>Refer to Doctor</h2>
-      <label>Specialty</label>
-      <select>
-        <option>Radiologist</option>
-        <option>Pediatrician</option>
-        <option>OB-GYN Specialist</option>
-        <option>Internist </option>
-        <option>General Medicine </option>
-        <option>Clinic </option>
-      </select>
-      <label>Doctor</label>
-      <select>
-        <option>Select</option>
-      </select>
-      <br />
-      <label>Remarks</label>
-      <textarea />
+      <div className="container px-3">
+        <div className="column">
+          <div className="col-sm">
+            <label>Specialty</label>
+            <select>
+              <option>Radiologist</option>
+              <option>Pediatrician</option>
+              <option>OB-GYN Specialist</option>
+              <option>Internist </option>
+              <option>General Medicine </option>
+              <option>Clinic </option>
+            </select>
+          </div>
+          <div className="col-sm">
+            <label>Doctor</label>
+            <select>
+              <option>Select</option>
+            </select>
+          </div>
+          <div className="col-sm">
+            <label>Remarks</label>
+            <textarea />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
